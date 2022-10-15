21362df8e09d828e390ec54e4642827b52a57e27
Your Name <moisesjuniorcas20@gmail.com>
2022-10-15 12:10:13 -0500

    probando importar componente

--- Proyecto/front/src/App.js
+++ Proyecto/front/src/App.js
@@ -1,10 +1,12 @@
 
 import React from 'react';
+import Ventasadmin from './components/Ventasadmin';
 
 function App() {
   return (
     <div className="App">
       <h1>Proyecto</h1>
+      <Ventasadmin />
     </div>
   );
 }
