cc8dddd9b63bdb5325a8e831d84a35a499f25eaa
Your Name <moisesjuniorcas20@gmail.com>
2022-10-21 15:01:57 -0500

    iniciando el back

---
+++ Proyecto/back/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express(); 
+
+app.use(express.json()); //para poder usar json en el body
+
+//importamos las rutas
+const products = require('./routes/products'); //importamos el archivo de rutas de productos
+
+//creamos las rutas
+app.use('/api', products); //creamos la ruta
+
+module.exports = app; //exportamos el modulo 
