b8ad10b2e6d91fcbf5c7ee0ee35d9dac03ea0e29
Your Name <moisesjuniorcas20@gmail.com>
2022-10-24 15:42:38 -0500

    se crea un menú visual de navegación

--- Proyecto/front/src/components/Header.js
+++ Proyecto/front/src/components/Header.js
@@ -19,6 +19,7 @@ const Header = () => {
             <Nav.Link href="/ventasadmin">Ventas</Nav.Link>
             <Nav.Link href="/productoscliente">Cliente</Nav.Link>
             <Nav.Link href="/productosadmin">Admin</Nav.Link> 
+            <Nav.Link href="/carrito">Carrito</Nav.Link> 
             
           </Nav>
         </Navbar.Collapse>
@@ -49,7 +50,7 @@ const Header = () => {
             <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
                 <span><button type="button" class="btn btn-warning">Iniciar sesión</button></span>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i><Link to={`/carrito`} ></Link>   
+                <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"><Link to={`/carrito`} ></Link></i>   
                 <span className="ml-1" id="cart_count">2</span>
             </div>
 
