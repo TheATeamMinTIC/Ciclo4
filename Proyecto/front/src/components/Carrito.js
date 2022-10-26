c2d0dcf9461faaa8b49a184de3c87fed40b8eae2
Your Name <moisesjuniorcas20@gmail.com>
2022-10-26 11:19:36 -0500

    agregando cosas para el sprint

--- Proyecto/front/src/components/Carrito.js
+++ Proyecto/front/src/components/Carrito.js
@@ -22,7 +22,7 @@ const Carrito = () => {
   return (
     <div>
     <Fragment>
-    <h1>Carrito HU_011 Andres V</h1>
+    <h1>Carrito</h1>
     <div class="container">
 		<h1><i class="fa fa-shopping-cart" aria-hidden="true"></i>Carrito</h1>
 		<hr/>
