37d2995ed243262919fde769a6c5be4be4f87f9f
lievDev <josephbarraza_03@outlook.com>
2022-10-30 09:08:53 -0500

    Adicion menu desplegable panel de control

--- Proyecto/front/src/components/Header.js
+++ Proyecto/front/src/components/Header.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom';
 
 
 
-
 const Header = () => {
   return (
     
@@ -51,8 +50,17 @@ const Header = () => {
                 </div>
             </div>
             <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                <span><button type="button" class="btn btn-warning">Iniciar sesión</button></span>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="ml-4 dropdown d-inline">
+                        <Link to="#!" className="btn dropdown-toggle text-white mr-4" type="button"
+                        id="dropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span>Panel de Control</span></Link>
+                            <div className='dropdown-menu' aria-labelledby='dropDownMenu'>
+                                <Link className="dropdown-item" to="/dashboard">Adm. Productos</Link>
+                                <Link className="dropdown-item" to="/">Pedidos</Link>
+                                <Link className="dropdown-item" to="/">Mi cuenta</Link>
+                                <Link className="dropdown-item" to="/">Cerrar Sesion</Link>
+                            </div>
+                    </div>
                 <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"><Link to={`/carrito`} ></Link></i>   
                 <span className="ml-1" id="cart_count">2</span>
             </div>
