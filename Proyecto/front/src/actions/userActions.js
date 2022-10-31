f20a0dde3ed5b578dad95654bd0a20035e2512fa
Your Name <moisesjuniorcas20@gmail.com>
2022-10-31 17:24:02 -0500

    se agrega userlist

--- Proyecto/front/src/actions/userActions.js
+++ Proyecto/front/src/actions/userActions.js
@@ -3,28 +3,47 @@ import axios from 'axios';
 import { ALL_USERS_REQUEST,
     ALL_USERS_SUCCESS,
     ALL_USERS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'; //importamos las constantes  
 
 // Get all products
-export const getUsers = (keyword = '', currentPage = 1, price, category, rating = 0) => async (dispatch) => { //dispatch es una funcion que me permite ejecutar una accion
-    try { //intente hacer esto
-        dispatch({ type: ALL_USERS_REQUEST }); //ejecutar la accion de tipo ALL_PRODUCTS_REQUEST
+export const getUsers = () => async (dispatch) => {
+    try {
+        dispatch({ type: ALL_USERS_REQUEST }); //enviamos la peticion
+        const { data } = await axios.get(`/api/usuarios`); //enviamos la peticion al backend
 
-        const {data} = await axios.get('api/usuarios') //obtener los productos de la base de datos')
-
-        dispatch({ //ejecutar la accion de tipo ALL_PRODUCTS_SUCCESS
+        dispatch({
             type: ALL_USERS_SUCCESS,
-            payload: data //paquete de productos
+            payload: data
         })
-
-        
-    }catch(error){
+    } catch (error) {
         dispatch({
             type: ALL_USERS_FAIL,
-            payload: error.response.data.message //mensaje de error
-        }) //ejecutar la accion de tipo ALL_PRODUCTS_FAIL
-    } //si algo sale mal haga esto 
+            payload: error.response.data.message
+        })
+    }
+}
+
+// Get product details
+export const getUserDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: USER_DETAILS_REQUEST }); //enviamos la peticion
+
+        const { data } = await axios.get(`/api/usuario/${id}`); //enviamos la peticion al backend
+
+        dispatch({
+            type: USER_DETAILS_SUCCESS,
+            payload: data.user
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
 }
 
 // Clear errors
--- Proyecto/front/src/reducer/userReducer.js
+++ Proyecto/front/src/reducer/userReducer.js
@@ -1,8 +1,11 @@
 import { ALL_USERS_REQUEST,
     ALL_USERS_SUCCESS,
     ALL_USERS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
     CLEAR_ERRORS
-} from '../constants/userConstants'; //importamos las constantes  
+} from '../constants/userConstants';//importamos las constantes  
 
 export const usersReducer = (state ={ users: []}, action)=>{
     switch(action.type){
@@ -22,6 +25,39 @@ export const usersReducer = (state ={ users: []}, action)=>{
         case ALL_USERS_FAIL:
             return{
                 loading:false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return{
+                ...state,
+                error:null
+            }
+        
+
+        default:
+            return state;
+    }
+}
+
+//reducer para obtener los detalles de un usuario
+export const userDetailsReducer = (state ={ user: {}}, action)=>{
+    switch(action.type){
+        case USER_DETAILS_REQUEST:
+            return{
+                ...state,
+                loading:true
+            }
+
+        case USER_DETAILS_SUCCESS:
+            return{
+                loading:false,
+                user: action.payload
+            }
+
+        case USER_DETAILS_FAIL:
+            return{
+                ...state,
                 error: action.payload
             }
 
