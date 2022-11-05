73758ed2175b473afc65bef8511229a933d7729c
lievDev <josephbarraza_03@outlook.com>
2022-11-04 22:49:04 -0500

    logout funcional

--- Proyecto/back/routes/auth.js
+++ Proyecto/back/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require ("express");
-const { newUser,getUsers,getUserById,updateUser,deleteUser, loginUser } = require("../controllers/authController");
+const { newUser,getUsers,getUserById,updateUser,deleteUser, loginUser, logOut } = require("../controllers/authController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.route('/usuario/registro').post(newUser);//creamos la ruta, post es para crear, route es para crear una ruta, newProduct es el metodo que vamos a ejecutar
@@ -10,5 +11,6 @@ router.route('/usuario/:id').put(updateUser); //creamos la ruta, put es para act
 router.route('/usuario/:id').delete(deleteUser); //creamos la ruta, delete es para eliminar, route es para crear una ruta, deleteProduct es el metodo que vamos a ejecutar, es necesario el :id para que sepa que es un parametro
 
 router.route('/login').get(loginUser)
+router.route('/logout').get(isAuthenticatedUser,logOut)
 
 module.exports = router;
