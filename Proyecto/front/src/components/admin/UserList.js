19d3237c9ae5d2bcb66eb524828e1c5b91dcfc18
Your Name <moisesjuniorcas20@gmail.com>
2022-11-04 21:28:33 -0500

    se intenta traer los usuarios por fecth

--- Proyecto/front/src/components/admin/UserList.js
+++ Proyecto/front/src/components/admin/UserList.js
@@ -1,35 +1,24 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react';
 import { MDBDataTable } from 'mdbreact'
-
-import MetaData from '../layout/MetaData'
 import Sidebar from './Sidebar'
+import MetaData from '../layout/MetaData'
 
-import { useDispatch, useSelector } from 'react-redux'
-import { getUsers } from '../../actions/userActions'
 import {Link } from "react-router-dom"
-
-
-//import { useAlert } from 'react-alert'
-
 
 
 
 export const UserList = () => {
     
-    const { loading, usuarios} = useSelector(state=> state.users)
-    //const alert= useAlert();
-    
-    const dispatch = useDispatch();
+    const [usuarios, setUsuarios] = useState([])
+
+ //fetch get http://localhost:4000/api/usuarios
     useEffect(() => {
-        // if (error){
-        //     return alert.error(error)
-        // }
+        fetch('http://localhost:4000/api/usuarios') //fetch es una funcion que recibe como parametro la url a la que quiero hacer la peticion
+        .then(res => res.json()) //res es la respuesta que me da el servidor, res.json() es para convertir la respuesta en un json
+        .then(data => setUsuarios(data)) //data es el json que me devuelve el servidor, setProducts es la funcion que me permite cambiar el estado de products
+    }, [])
 
-        dispatch(getUsers()); //llamamos a la accion de userActions para obtener los usuarios de la base de datos y los guardamos en el state de redux users 
-    }, [dispatch])
-
-
-    const setUser = () => { //funcion para mostrar los usuarios 
+    setUsuarios = () => { //funcion para mostrar los productos en la tabla
         const data = { //data es un objeto
             columns: [ //se reemplaza luego por la data de la base de datos
                 {
@@ -43,20 +32,11 @@ export const UserList = () => {
                     sort: 'asc'
                 },
                 {
-                    label: 'Password',
-                    field: 'password',
-                    sort: 'asc'
-                },
-                {
-                    label: 'Avatar',
-                    field: 'avatar',
-                    sort: 'asc'
-                },
-                {
                     label: 'Role',
                     field: 'role',
                     sort: 'asc'
                 },
+                
                 {
                     label: 'Acciones',
                     field: 'actions',
@@ -64,59 +44,61 @@ export const UserList = () => {
             ],
             rows: [] //tantas filas como productos haya en la base de datos
         }
-        usuarios.map(function(user) { //recorro todos los usuarios por cada usuario que encuentre voy a crear una fila
-            data.rows.push({ //agrego una fila a la tabla con los datos del usuario que estoy recorriendo en ese momento
-                name: user.name,
-                email: user.email,
-                password: user.password,
-                avatar: user.avatar,
-                role: user.role,
+        usuarios.forEach(usuario => { //recorro todos los productos por cada producto que encuentre voy a crear una fila
+            data.rows.push({ //agrego una fila a la tabla con los datos del producto que estoy recorriendo en ese momento
+                name: usuario.name,
+                email: usuario.email,
+                role: usuario.role,
                 actions: <Fragment> {/* fragment es un contenedor que no se muestra en el html */}
-                    <Link to={`/usuarios/${user._id}`} className="btn btn-primary py-1 px-2">
-                        <i className="fa fa-pencil"></i>
+                    <Link to={`/admin/product/${usuario._id}`} className="btn btn-primary py-1 px-2">
+                        <i className="fa fa-eye"></i>
+                    </Link><Link to="/" className="btn btn-warning py-1 px-2">
+                    <i class="fa fa-pencil"></i>
                     </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2">
+                    
+                    <Link to="/" className="btn btn-danger py-1 px-2">
                         <i className="fa fa-trash"></i>
-                    </button>
+                    </Link>
+
                 </Fragment>
             })
         })
 
-        return data;
+        return data; //devuelvo la data con los productos
 
+        
     }
 
     
-        
+
 
   return (
     <Fragment>
-            <MetaData title={'All Users'} />
-            <div className="row">
-                <div className="col-12 col-md-2">
-                    <Sidebar /> {/* sidebar requerido*/}
-                </div>
-
-                <div className="col-12 col-md-10">
-                    <Fragment>
-                        <h1 className="my-5">Usuarios Registrados</h1>
-
-                        {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
-                            <MDBDataTable //tabla de mdbreact para mostrar los productos
-                                data={setUser()} //le paso la data que cree con la funcion setProducts
-                                className="px-3"
-                                bordered //borde de la tabla
-                                striped //filas alternadas
-                                hover   //efecto hover en las filas de la tabla 
-                            />
-                        )}
-
-                    </Fragment>
-                </div>
+        <MetaData title={'All Products'} />
+        <div className="row">
+            <div className="col-12 col-md-2">
+                <Sidebar />
             </div>
 
-        </Fragment>
+            <div className="col-12 col-md-10">
+                <Fragment>
+                    <h1 className="my-5">All Products</h1>
+
+                    <MDBDataTable
+                        data={setUsuarios()}
+                        className="px-3"
+                        bordered
+                        striped
+                        hover
+                    />
+                </Fragment>
+            </div>
+        </div>
+    </Fragment>
+
   )
 }
+
+
 
 export default UserList
